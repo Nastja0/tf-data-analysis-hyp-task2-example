7fbaee9d5f9e7cd3704580c95e7c1be319d8e04c
Nastja0 <anastasizta_mgn3003@inbox.ru>
2023-04-12 17:31:31 +0500

    update

--- solution.py
+++ solution.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
-from hyppo.ksample import MMD
-
+from scipy.stats import cramervonmises_2samp
 
 chat_id = 522929689 # Ваш chat ID, не меняйте название переменной
 
@@ -9,5 +8,5 @@ def solution(x: np.array, y: np.array) -> bool:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    p_value = 0.008
-    return MMD(compute_kernel="laplacian", gamma=1).test(x, y)[1] <=  p_value# Ваш ответ, True или False
+    p_value = 0.08
+    return cramervonmises_2samp(x, y).pvalue < p_value# Ваш ответ, True или False
